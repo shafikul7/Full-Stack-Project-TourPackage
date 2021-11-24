dc04972b3c3bc1feb2a56cfd946f20362bd0f62b
Md.Shafikul Islam <63946398+mdshafikulislam@users.noreply.github.com>
2021-11-24 22:05:21 +0600

    Update index.js

--- TourPackage frontend/src/index.js
+++ TourPackage frontend/src/index.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import '../node_modules/font-awesome/css/font-awesome.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import App from './App';
 import './index.css';
@@ -14,7 +13,4 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
